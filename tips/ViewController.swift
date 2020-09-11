33ca2ce3cd3bd985deafb99be31431c5678115ee
Nabib Ahmed <nahmed3536@gmail.com>
2020-09-11 12:54:35 -0400

    Initial Commit

---
+++ tips/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tips
+//
+//  Created by Nabib Ahmed on 9/11/20.
+//  Copyright © 2020 CodePath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
